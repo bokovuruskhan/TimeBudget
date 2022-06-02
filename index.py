b64b43ce562e655d48143a6593465679ab5d1f7b
Uruskhan Bokov <bokovuruskhan@gmail.com>
2022-06-02 11:24:24 +0300

    im too tired to describe it :)

--- index.py
+++ index.py
@@ -1,11 +1,28 @@
+from datetime import datetime
+from flask_login import login_required, LoginManager, logout_user, login_user, current_user
 from flask import render_template, request
-from model import Week, Todo
+from model import Week, Todo, save, find_by_id, delete, get_all, Category, User
 import admin
 from config import MyApp
 
 app = MyApp.app
 database = MyApp.database
 babel = MyApp.babel
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "/login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return database.session.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return login()
 
 
 @babel.localeselector
@@ -14,17 +31,94 @@ def get_locale():
 
 
 @app.route("/todo/add", methods=["POST"])
+@login_required
 def add_todo():
     day_id = request.form.get("day_id")
     name = request.form.get("name")
-    Todo.add(name, day_id)
+    end_time = request.form.get("end_time")
+    category_id = request.form.get("category_id")
+    if end_time == "":
+        end_time = None
+    else:
+        end_time = datetime.strptime(end_time, "%H:%M").time()
+    Todo.add(name, day_id, end_time, category_id=category_id)
     return index()
 
 
+@app.route("/todo/complete", methods=["POST"])
+@login_required
+def complete_todo():
+    todo_id = request.form.get("todo_id")
+    todo = find_by_id(todo_id, Todo)
+    todo.open = False
+    save(todo)
+    return index()
+
+
+@app.route("/todo/delete", methods=["POST"])
+@login_required
+def delete_todo():
+    todo_id = request.form.get("todo_id")
+    todo = find_by_id(todo_id, Todo)
+    delete(todo)
+    return index()
+
+
+@app.route("/category/add", methods=["POST"])
+@login_required
+def add_category():
+    name = request.form.get("name")
+    Category.add(name,current_user)
+    return index()
+
+
+@app.route("/week/<week_id>/show")
+@login_required
+def show_week(week_id):
+    return index(week_days=find_by_id(int(week_id), Week).get_week_days())
+
+
+@app.route("/categories")
+@login_required
+def categories():
+    return render_template("categories.html", categories=get_all(Category))
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.find_by_username(username)
+        if user and user.check_password(password):
+            login_user(user, remember=True)
+            return index()
+    return render_template("login.html")
+
+
+@app.route("/registration", methods=["POST", "GET"])
+def registration():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        password_again = request.form.get("password_again")
+        user = User.find_by_username(username)
+        if user is None and password == password_again:
+            user = User(username=username)
+            user.set_password(password)
+            save(user)
+            login_user(user, remember=True)
+            return index()
+    return render_template("registration.html")
+
+
 @app.route("/")
-def index():
-    Week.init_week()
-    return render_template("index.html", week=Week.get_current_week())
+@login_required
+def index(week_days=None):
+    if week_days is None:
+        week_days = current_user.get_current_week().get_week_days()
+    print(week_days)
+    return render_template("index.html", user=current_user, week_days=week_days)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -1,7 +1,48 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from config import MyApp
 from datetime import datetime, timedelta
 
 database = MyApp.database
+
+
+class User(database.Model, UserMixin):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(255), unique=True, nullable=False)
+    password = database.Column(database.String(255), unique=True, nullable=False)
+    weeks = database.relationship("Week",
+                                  backref=database.backref("user", lazy=True))
+    categories = database.relationship("Category",
+                                       backref=database.backref("user", lazy=True))
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    def get_my_week_by_id(self, week_id):
+        for week in self.weeks:
+            if week.id == week_id:
+                return week
+
+    def get_current_week(self):
+        today = datetime.today().date()
+        current_week_start_date = today - timedelta(days=today.weekday())
+        for week in self.weeks:
+            if week.start_date == current_week_start_date:
+                return week
+        week = Week(start_date=current_week_start_date, user=self)
+        for i in range(7):
+            week.days.append(Day(date=current_week_start_date))
+            current_week_start_date += timedelta(days=1)
+        save(week)
+        return self.get_current_week()
+
+    @staticmethod
+    def find_by_username(username):
+        return database.session.query(User).filter(User.username == username).first()
 
 
 class Week(database.Model):
@@ -9,31 +50,14 @@ class Week(database.Model):
     start_date = database.Column(database.Date, nullable=False)
     days = database.relationship("Day",
                                  backref=database.backref("week", lazy=True))
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
 
-    @staticmethod
-    def find_by_start_date(start_date):
-        return database.session.query(Week).filter(Week.start_date == start_date).first()
-
-    @staticmethod
-    def init_week():
-        today = datetime.today().date()
-        current_week_start_date = today - timedelta(days=today.weekday())
-        if Week.find_by_start_date(current_week_start_date) is None:
-            week = Week(start_date=current_week_start_date)
-            for i in range(7):
-                week.days.append(Day(date=current_week_start_date))
-                current_week_start_date += timedelta(days=1)
-            database.session.add(week)
-            database.session.commit()
-
-    @staticmethod
-    def get_current_week():
+    def get_week_days(self):
         week = {}
-        today = datetime.today().date()
-        date = today - timedelta(days=today.weekday() + 1)
+        date = self.start_date
         for i in range(7):
+            week.update({date: self.days[i]})
             date += + timedelta(days=1)
-            week.update({date: Day.find_by_date(date)})
         return week
 
 
@@ -41,7 +65,7 @@ class Day(database.Model):
     day_names = ["ПОНЕДЕЛЬНИК", "ВТОРНИК", "СРЕДА", "ЧЕТВЕРГ", "ПЯТНИЦА", "СУББОТА", "ВОСКРЕСЕНЬЕ"]
 
     id = database.Column(database.Integer, primary_key=True)
-    date = database.Column(database.Date, unique=True)
+    date = database.Column(database.Date)
     week_id = database.Column(database.Integer, database.ForeignKey('week.id'))
     todos = database.relationship("Todo",
                                   backref=database.backref("day", lazy=True))
@@ -54,12 +78,44 @@ class Day(database.Model):
         return database.session.query(Day).filter(Day.date == date).first()
 
 
+class Category(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(255), nullable=False)
+    todos = database.relationship("Todo",
+                                  backref=database.backref("category", lazy=True))
+    user_id = database.Column(database.Integer, database.ForeignKey('user.id'), nullable=False)
+
+    @staticmethod
+    def add(name, user):
+        save(Category(name=name, user=user))
+
+
 class Todo(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(255), nullable=False)
+    open = database.Column(database.Boolean, default=True, nullable=False)
+    end_time = database.Column(database.Time, nullable=True)
     day_id = database.Column(database.Integer, database.ForeignKey('day.id'))
+    category_id = database.Column(database.Integer, database.ForeignKey('category.id'), nullable=False)
 
     @staticmethod
-    def add(name, day_id):
-        database.session.add(Todo(name=name, day_id=day_id))
-        database.session.commit()
+    def add(name, day_id, end_time, category_id):
+        save(Todo(name=name, day_id=day_id, end_time=end_time, category_id=category_id))
+
+
+def save(obj):
+    database.session.add(obj)
+    database.session.commit()
+
+
+def delete(obj):
+    database.session.delete(obj)
+    database.session.commit()
+
+
+def get_all(obj_class):
+    return database.session.query(obj_class).all()
+
+
+def find_by_id(obj_id, obj_class):
+    return database.session.query(obj_class).filter(obj_class.id == obj_id).first()
